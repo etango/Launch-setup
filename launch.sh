737890041be71b4634878f5955d1c0f289cc1741
Elton Tang <etang315@gmail.com>
2015-10-04 21:23:07 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-Aws ec2 run-instances  --image-id $1 --count $2 --instance-type $3  --security-group-ids $4--subnet-id $5 --key-name $6 --associate-public-ip-address -user-data file://etango/install-env.sh
+Aws ec2 run-instances  --image-id $1 --count $2 --instance-type $3  --security-group-ids $4--subnet-id $5 --key-name $6 --associate-public-ip-address -user-data file://../Environment-setup/install-env.sh
