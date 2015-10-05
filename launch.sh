07fe47a9b5a930c86f2bfedff6c414c98140fae4
Elton Tang <etang315@gmail.com>
2015-10-04 21:15:36 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-Aws ec2 run-instances  --image-id $1 --count $2 --instance-type $3  --security-group-ids $4--subnet-$5 --key-name $6 --associate-public-ip-address
+Aws ec2 run-instances  --image-id $1 --count $2 --instance-type $3  --security-group-ids $4--subnet-$5 --key-name $6 --associate-public-ip-address -user-data file://install-env.sh
